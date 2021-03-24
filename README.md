7a298a1333b67fb34699d34087deb734c3958cf3
mroohian <m.roohian87@gmail.com>
2021-03-24 19:56:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-shwexo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-shwexo)
---
+++ index.ts
@@ -0,0 +1,33 @@
+import { getInput, setInput, showResult } from "./utils";
+
+function counterFunction(): void {
+  const value: string = getInput();
+
+  const valueParts: string[] = value.split(':');
+
+  let hours: number = parseInt(valueParts[0]);
+  let minutes: number = parseInt(valueParts[1]);
+  let seconds: number = parseInt(valueParts[2]);
+
+  seconds = seconds + 1;
+
+  if (seconds > 59) {
+    seconds = 0;
+    minutes = minutes + 1;
+  }
+
+  if (minutes > 59) {
+    minutes = 0;
+    hours = hours + 1;
+  }
+
+  const output = `${hours}:${minutes}:${seconds}`
+
+  setInput(output);
+  showResult(output);
+}
+
+setInput('12:00:00');
+
+setInterval(counterFunction, 5);
+// setTimeout(counterFunction, 50);
